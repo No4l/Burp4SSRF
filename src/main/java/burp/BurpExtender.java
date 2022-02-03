9d12b6af21edce7a6b9e92e67e4b70af3771fb43
yubanbanz <yubanbanz@gmail.com>
2022-02-03 20:05:11 +0800

    host && uri

---
+++ src/main/java/burp/BurpExtender.java
@@ -0,0 +1,335 @@
+package burp;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class BurpExtender implements IBurpExtender {
+    private static final String name = "Burp4SSRF";
+    private static final String version = "Beta";
+
+    // provides potentially useful info but increases memory usage
+    static final boolean SAVE_RESPONSES = false;
+
+
+    @Override
+    public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks) {
+        new Utilities(callbacks);
+        callbacks.setExtensionName(name);
+
+        Correlator collab = new Correlator();
+
+        Monitor collabMonitor = new Monitor(collab);
+        new Thread(collabMonitor).start();
+        callbacks.registerExtensionStateListener(collabMonitor);
+
+        callbacks.registerScannerCheck(new Injector(collab));
+
+        Utilities.out("Loaded " + name + " v" + version);
+    }
+}
+
+class Monitor implements Runnable, IExtensionStateListener {
+    private Correlator collab;
+    private boolean stop = false;
+
+    Monitor(Correlator collab) {
+        this.collab = collab;
+    }
+
+    public void extensionUnloaded() {
+        Utilities.out("Extension unloading - triggering abort");
+        stop = true;
+        Thread.currentThread().interrupt();
+    }
+
+    public void run() {
+        try {
+            while (!stop) {
+                Thread.sleep(10000);
+                collab.poll().forEach(e -> processInteraction(e));
+            }
+        }
+        catch (InterruptedException e) {
+            Utilities.out("Interrupted");
+        }
+        catch (Exception e) {
+            Utilities.out("Error fetching/handling interactions: "+e.getMessage());
+        }
+
+        Utilities.out("Shutting down collaborator monitor thread");
+    }
+
+    private void processInteraction(IBurpCollaboratorInteraction interaction) {
+        String id = interaction.getProperty("interaction_id");
+        Utilities.out("Got an interaction:"+interaction.getProperties());
+        MetaRequest metaReq = collab.getRequest(id);
+        IHttpRequestResponse req = metaReq.getRequest();
+        String type = collab.getType(id);
+        String severity = "High";
+        String ipAddress = interaction.getProperty("client_ip");
+
+        /*
+        if(ipAddress.startsWith("74.125.")){
+            return;
+        }
+        */
+
+        String rawDetail = interaction.getProperty("request");
+        if (rawDetail == null) {
+            rawDetail = interaction.getProperty("conversation");
+        }
+
+        if (rawDetail == null) {
+            severity = "Medium";
+            rawDetail = interaction.getProperty("raw_query");
+        }
+
+        String message = "The collaborator was contacted by <b>" + ipAddress;
+
+        try {
+            String reverseDns = InetAddress.getByName(ipAddress).getCanonicalHostName();
+            if (!ipAddress.equals(reverseDns)) {
+                message += " ("+reverseDns +")";
+            }
+        }
+        catch (UnknownHostException e) {
+            message += " (reverse dns lookup failed)";
+        }
+        message +=  "</b>";
+
+        try {
+            long interactionTime = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss z").parse(interaction.getProperty("time_stamp")).getTime();
+            long mill = interactionTime - metaReq.getTimestamp();
+            int seconds = (int) (mill / 1000) % 60;
+            int minutes = (int) ((mill / (1000 * 60)) % 60);
+            int hours = (int) ((mill / (1000 * 60 * 60)) % 24);
+            message += " after a delay of <b>" + String.format("%02d:%02d:%02d", hours, minutes, seconds) + "</b>:<br/><br/>";
+        }
+        catch (java.text.ParseException e) {
+            message += e.toString();
+        }
+
+        if (collab.isClientIP(interaction.getProperty("client_ip"))) {
+            message += "<b>This interaction appears to have been issued by your IP address</b><br/><br/>";
+            severity = "Low";
+        }
+
+        String decodedDetail = new String(Utilities.helpers.base64Decode(rawDetail));
+        message += "<pre>    "+decodedDetail.replace("<", "&lt;").replace("\n", "\n    ")+"</pre>";
+
+        message += "The payload was sent at "+new Date(metaReq.getTimestamp()).toString() + " and received on " + interaction.getProperty("time_stamp") +"<br/><br/>";
+
+        message += "To manually replicate this issue, use the Burp Collaborator Client available via the 'Burp' menu on the top left.<br/><br/>";
+
+        IRequestInfo reqInfo = Utilities.callbacks.getHelpers().analyzeRequest(req.getHttpService(), req.getRequest());
+        Utilities.callbacks.addScanIssue(
+                new CustomScanIssue(req.getHttpService(), reqInfo.getUrl(), new IHttpRequestResponse[]{req}, "Collaborator Pingback ("+interaction.getProperty("type")+"): "+type, message+interaction.getProperties().toString(), severity, "Certain", "Panic"));
+    }
+
+}
+
+class MetaRequest {
+    private IHttpRequestResponse request;
+    private static int burpId = 0;
+    private long timestamp;
+
+    MetaRequest(IHttpRequestResponse proxyMessage) {
+        request = proxyMessage;
+        burpId++;
+        timestamp = System.currentTimeMillis();
+    }
+
+    public void overwriteRequest(IHttpRequestResponse response) {
+        request = response;
+    }
+
+    public IHttpRequestResponse getRequest() {
+        return request;
+    }
+
+    public int getBurpId() {
+        return burpId;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
+
+class Correlator {
+
+    private IBurpCollaboratorClientContext collab;
+    private HashMap<String, Integer> idToRequestID;
+    private HashMap<String, String> idToType;
+    private HashMap<Integer, MetaRequest> requests;
+    private HashMap<Integer, Integer> burpIdToRequestID;
+    private HashSet<String> client_ips;
+    private int count = 0;
+
+    Correlator() {
+        idToRequestID = new HashMap<>();
+        requests = new HashMap<>();
+        idToType = new HashMap<>();
+        burpIdToRequestID = new HashMap<>();
+        collab = Utilities.callbacks.createBurpCollaboratorClientContext();
+        client_ips = new HashSet<>();
+
+        try {
+            String pollPayload = collab.generatePayload(true);
+            Utilities.callbacks.makeHttpRequest(pollPayload, 80, false, ("GET / HTTP/1.1\r\nHost: " + pollPayload + "\r\n\r\n").getBytes());
+            for (IBurpCollaboratorInteraction interaction: collab.fetchCollaboratorInteractionsFor(pollPayload)) {
+                client_ips.add(interaction.getProperty("client_ip"));
+            }
+            Utilities.out("Calculated your IPs: "+ client_ips.toString());
+        }
+        catch (NullPointerException e) {
+            Utilities.out("Unable to calculate client IP - collaborator may not be functional");
+        }
+        catch (java.lang.IllegalArgumentException e) {
+            Utilities.out("The Collaborator appears to be misconfigured. Please run a health check via Project Options->Misc. Also, note that Collaborator Everywhere does not support the IP-address mode.");
+        }
+
+    }
+
+    java.util.List<IBurpCollaboratorInteraction> poll() {
+        return collab.fetchAllCollaboratorInteractions();
+    }
+
+    Integer addRequest(MetaRequest req) {
+        Integer requestCode = count++;
+        requests.put(requestCode, req);
+        burpIdToRequestID.put(req.getBurpId(), requestCode);
+        return requestCode;
+    }
+
+    String generateCollabId(int requestCode, String type) {
+        String id = collab.generatePayload(false);
+        idToRequestID.put(id, requestCode);
+        idToType.put(id, type);
+        return id+"."+collab.getCollaboratorServerLocation();
+    }
+
+    String getLocation() {
+        return collab.getCollaboratorServerLocation();
+    }
+
+    boolean isClientIP(String ip){
+        return client_ips.contains(ip);
+    }
+
+    MetaRequest getRequest(String collabId) {
+        int requestId = idToRequestID.get(collabId);
+        return requests.get(requestId);
+    }
+
+    void updateResponse(int burpId, IHttpRequestResponse response) {
+        if (burpIdToRequestID.containsKey(burpId)) {
+            requests.get(burpIdToRequestID.get(burpId)).overwriteRequest(response);
+        }
+    }
+
+    String getType(String collabid) {
+        return idToType.get(collabid);
+    }
+}
+
+
+
+class Injector implements IScannerCheck{
+    private Correlator collab;
+    HashSet<String[]> injectionPoints = new HashSet<>();
+
+
+    Injector(Correlator collab) {
+        this.collab = collab;
+
+        Scanner s = new Scanner(getClass().getResourceAsStream("/injections"));
+        while (s.hasNextLine()) {
+            String injection = s.nextLine();
+            if (injection.charAt(0) == '#') {
+                continue;
+            }
+            injectionPoints.add(injection.split(",", 3));
+        }
+        s.close();
+
+    }
+
+    public byte[] injectPayloads(byte[] request, Integer requestCode) {
+
+        //request = Utilities.replaceRequestLine(request, "GET @"+collabId + "/"+collabId.split("[.]")[0] + " HTTP/1.1");
+        //request = Utilities.addOrReplaceHeader(request, "Referer", "http://portswigger-labs.net/redirect.php?url=https://portswigger-labs.net/"+collabId);
+
+        request = Utilities.addOrReplaceHeader(request, "Cache-Control", "no-transform");
+
+        for (String[] injection: injectionPoints) {
+            if(injection[0].equals("raw") || injection[0].equals("host")){
+                continue;
+            }
+            String payload = injection[2].replace("%s", collab.generateCollabId(requestCode, injection[1]));
+            // replace %h with corresponding Host header (same as with %s for Collaborator)
+            payload = payload.replace("%h", Utilities.getHeader(request, "Host"));
+            switch ( injection[0] ){
+                case "param":
+                    IParameter param = Utilities.helpers.buildParameter(injection[1], payload, IParameter.PARAM_URL);
+                    request = Utilities.helpers.removeParameter(request, param);
+                    request = Utilities.helpers.addParameter(request, param);
+                    break;
+
+                case "header":
+                    request = Utilities.addOrReplaceHeader(request, injection[1], payload);
+                    break;
+
+            }
+
+        }
+
+        return request;
+    }
+
+    public void injectAnyWhere(byte[] request, Integer requestCode, IHttpRequestResponse iHttpRequestResponse) {
+        request = Utilities.addOrReplaceHeader(request, "Cache-Control", "no-transform");
+
+        for (String[] injection: injectionPoints) {
+            String payload = injection[1].replace("%s", collab.generateCollabId(requestCode, injection[1]));
+            // replace %h with corresponding Host header (same as with %s for Collaborator)
+            payload = payload.replace("%h", Utilities.getHeader(request, "Host"));
+            switch ( injection[0] ){
+                case "raw":
+                    Utilities.callbacks.makeHttpRequest(iHttpRequestResponse.getHttpService(),Utilities.replaceRequestLine(request,  payload));
+
+                    break;
+
+                case "host":
+                    Utilities.callbacks.makeHttpRequest(iHttpRequestResponse.getHttpService(),Utilities.addOrReplaceHeader(request, "Host",payload));
+                    break;
+            }
+
+
+        }
+    }
+    @Override
+    public List<IScanIssue> doPassiveScan(IHttpRequestResponse iHttpRequestResponse) {
+        if (iHttpRequestResponse.getHttpService().getHost().endsWith(collab.getLocation())) {
+            return null;
+        }
+
+        MetaRequest req = new MetaRequest(iHttpRequestResponse);
+        Integer requestCode = collab.addRequest(req);
+        Utilities.callbacks.makeHttpRequest(iHttpRequestResponse.getHttpService(),injectPayloads(iHttpRequestResponse.getRequest(), requestCode));
+        injectAnyWhere(iHttpRequestResponse.getRequest(), requestCode,iHttpRequestResponse);
+        return null;
+    }
+
+    @Override
+    public List<IScanIssue> doActiveScan(IHttpRequestResponse iHttpRequestResponse, IScannerInsertionPoint iScannerInsertionPoint) {
+        return null;
+    }
+
+    @Override
+    public int consolidateDuplicateIssues(IScanIssue iScanIssue, IScanIssue iScanIssue1) {
+        return 0;
+    }
+}
---
+++ src/main/java/burp/Utilities.java
@@ -0,0 +1,248 @@
+package burp;
+
+import burp.IBurpExtenderCallbacks;
+import burp.IExtensionHelpers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Utilities {
+    private static PrintWriter stdout;
+    private static PrintWriter stderr;
+    public static IBurpExtenderCallbacks callbacks;
+    public static IExtensionHelpers helpers;
+    public static String a;
+
+    public Utilities(final IBurpExtenderCallbacks incallbacks) {
+        callbacks = incallbacks;
+        helpers = callbacks.getHelpers();
+        stdout = new PrintWriter(callbacks.getStdout(), true);
+        stderr = new PrintWriter(callbacks.getStderr(), true);
+    }
+
+    public static void out(String message) {
+        stdout.println(message);
+    }
+    public static void err(String message) {
+        stderr.println(message);
+    }
+
+    public static byte[] replaceRequestLine(byte[] request, String payload) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            int i = 0;
+            while (request[i++] != '\n') {
+            }
+
+            outputStream.write(payload.getBytes());
+            outputStream.write(Arrays.copyOfRange(request, i-2, request.length));
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Request creation unexpectedly failed");
+        }
+        return outputStream.toByteArray();
+    }
+
+    public static String getPath(byte[] request) {
+        int i = 0;
+        String out = "";
+        while (request[i++] != ' ') {
+        }
+        while (request[i] != ' ' && request[i] != '?') {
+            out += (char) request[i];
+            i++;
+        }
+        return out.substring(0, out.lastIndexOf('/'));
+    }
+
+
+    public static String getHeader(byte[] request, String header) {
+        int[] offsets = getHeaderOffsets(request, header);
+        String value = helpers.bytesToString(Arrays.copyOfRange(request, offsets[1], offsets[2]));
+        return value;
+    }
+
+    public static int[] getHeaderOffsets(byte[] request, String header) {
+        int i = 0;
+        int end = request.length;
+        while (i < end) {
+            int line_start = i;
+            while (i < end && request[i++] != ' ') {
+            }
+            byte[] header_name = Arrays.copyOfRange(request, line_start, i - 2);
+            int headerValueStart = i;
+            while (i < end && request[i++] != '\n') {
+            }
+            if (i == end) {
+                break;
+            }
+
+            if (i + 2 < end && request[i] == '\r' && request[i + 1] == '\n') {
+                break;
+            }
+
+            String header_str = helpers.bytesToString(header_name);
+
+            if (header.equals(header_str)) {
+                int[] offsets = {line_start, headerValueStart, i - 2};
+                return offsets;
+            }
+        }
+        return null;
+    }
+
+    // this is why hackxor2 is using python
+    // todo refactor to use getHeaderOffsets
+    public static byte[] addOrReplaceHeader(byte[] request, String header, String value) {
+        try {
+            int i = 0;
+            int end = request.length;
+            while (i < end && request[i++] != '\n') {
+            }
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+            while (i < end) {
+                int line_start = i;
+                while (i < end && request[i++] != ' ') {
+                }
+                byte[] header_name = Arrays.copyOfRange(request, line_start, i - 2);
+                int headerValueStart = i;
+                while (i < end && request[i++] != '\n') {
+                }
+                if (i == end) {
+                    break;
+                }
+
+                if(i+2<end && request[i] == '\r' && request[i+1] == '\n') {
+                    outputStream.write(Arrays.copyOfRange(request, 0, i));
+                    outputStream.write(helpers.stringToBytes(header + ": " + value+"\r\n"));
+                    outputStream.write(Arrays.copyOfRange(request, i, end));
+                    return outputStream.toByteArray();
+                }
+
+                String header_str = helpers.bytesToString(header_name);
+
+                if (header.equals(header_str)) {
+
+                    outputStream.write(Arrays.copyOfRange(request, 0, headerValueStart));
+                    outputStream.write(helpers.stringToBytes(value));
+                    outputStream.write(Arrays.copyOfRange(request, i-2, end));
+                    return outputStream.toByteArray();
+                }
+            }
+            outputStream.write(Arrays.copyOfRange(request, 0, end-2));
+            outputStream.write(helpers.stringToBytes(header + ": " + value+"\r\n\r\n"));
+            return outputStream.toByteArray();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Request creation unexpectedly failed");
+        }
+    }
+
+}
+
+class CustomScanIssue implements IScanIssue {
+    private IHttpService httpService;
+    private URL url;
+    private IHttpRequestResponse[] httpMessages;
+    private String name;
+    private String detail;
+    private String severity;
+    private String confidence;
+    private String remediation;
+
+    CustomScanIssue(
+            IHttpService httpService,
+            URL url,
+            IHttpRequestResponse[] httpMessages,
+            String name,
+            String detail,
+            String severity,
+            String confidence,
+            String remediation) {
+        this.name = name;
+        this.detail = detail;
+        this.severity = severity;
+        this.httpService = httpService;
+        this.url = url;
+        this.httpMessages = httpMessages;
+        this.confidence = confidence;
+        this.remediation = remediation;
+    }
+
+    @Override
+    public URL getUrl() {
+        return url;
+    }
+
+    @Override
+    public String getIssueName() {
+        return name;
+    }
+
+    @Override
+    public int getIssueType() {
+        return 0;
+    }
+
+    @Override
+    public String getSeverity() {
+        return severity;
+    }
+
+    @Override
+    public String getConfidence() {
+        return confidence;
+    }
+
+    @Override
+    public String getIssueBackground() {
+        return null;
+    }
+
+    @Override
+    public String getRemediationBackground() {
+        return null;
+    }
+
+    @Override
+    public String getIssueDetail() {
+        return detail;
+    }
+
+    @Override
+    public String getRemediationDetail() {
+        return remediation;
+    }
+
+    @Override
+    public IHttpRequestResponse[] getHttpMessages() {
+        return httpMessages;
+    }
+
+    @Override
+    public IHttpService getHttpService() {
+        return httpService;
+    }
+
+    //    @Override
+    public String getHost() {
+        return null;
+    }
+
+    //    @Override
+    public int getPort() {
+        return 0;
+    }
+
+    //    @Override
+    public String getProtocol() {
+        return null;
+    }
+
+}
